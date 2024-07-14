fef45bdb75abeba5bc9019bf4da5ee07545bf96a
HussainALAradi5 <hussainaradi.ha@gmail.com>
2024-07-14 09:13:27 +0300

    change the title with useEffect whenever the counter or the color change

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
-  const [counter, setCounter] = useState(0); //we initlize the counter value to 0
-  const add=()=>{setCounter(counter+1)}
-  const subtraction=()=>{
-    if(counter>0)
-    setCounter(counter-1)
-  }
+  const [counter, setCounter] = useState(0); // initialize the counter value to 0
+  const [color, setColor] = useState('royalblue');
+
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
+  const subtraction = () => {
+    if (counter > 0) setCounter(counter - 1);
+  };
+
+  const changeColor = () => {
+    setColor(color === 'royalblue' ? 'purple' : 'royalblue');
+  }; // change the color of the header
+
+  useEffect(() => {
+    document.title = `Count:  ${counter}${color}`;
+  }, [counter,color]); // change this to [color] to update text when color changes
+
   return (
     <div className="counter">
-      <h1 className="counterHeader">Counter:{counter}</h1>
-      <button className="add" onClick={add}>+1</button>
-      <button className="subtraction" onClick={subtraction}>-1</button>
+      <h1 className="counterHeader" style={{ color: color }}>Counter: {counter}</h1>
+      <br />
+      <button className="add" onClick={add}>
+        +1
+      </button>
+      <button className="subtraction" onClick={subtraction}>
+        -1
+      </button>
+      <button className="changeColor" onClick={changeColor}>Change Color</button>
     </div>
   );
 };
+
 export default Counter;
